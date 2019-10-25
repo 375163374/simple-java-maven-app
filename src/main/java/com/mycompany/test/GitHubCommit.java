a55729722bf597ef2ddc2870a7caa6215b45143b
linsb15 <linsb15@chinaunicom.cn>
2019-10-25 09:01:56 +0800

    first commit

---
+++ src/main/java/com/mycompany/test/GitHubCommit.java
@@ -0,0 +1,12 @@
+package com.mycompany.test;
+
+public class GitHubCommit {
+
+    public static void commitGit1(){
+        String str1="first commit";
+
+        System.out.println(str1);
+    }
+
+
+}
