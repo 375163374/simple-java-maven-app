6890f35b3f961fe192fbf0b4e52209eebfd5706c
linsb15 <linsb15@chinaunicom.cn>
2019-10-25 09:12:14 +0800

    Second submission

--- src/main/java/com/mycompany/test/GitHubCommit.java
+++ src/main/java/com/mycompany/test/GitHubCommit.java
@@ -4,6 +4,7 @@ public class GitHubCommit {
 
     public static void commitGit1(){
         String str1="first commit";
+        String str2="Second submission";
 
         System.out.println(str1);
     }
